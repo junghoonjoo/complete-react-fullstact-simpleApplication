2bf4063421965e4daf65e8ef993d0d71e0ea84ce
junghoonjoo <junghoonjoo@gmail.com>
2023-01-23 22:36:01 +0900

    Add simple Form to player

--- src/src/components/Stage_1.js
+++ src/src/components/Stage_1.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React,{useState,useContext,useRef} from 'react'
+
+import {Button, Form, Alert } from 'react-bootstrap';
 
 const Stage_1 = () => {
+
+  const textInput=useRef();
+
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const value=textInput.current.value;
+    console.log(value);
+  }
   return (
-    <div>Stage_1</div>
+    <>
+      <Form onSubmit={handleSubmit} className='mt-4'>
+        <Form.Group>
+          <Form.Control 
+            type='text' 
+            placeholder='Add plaer name' 
+            name='player' 
+            ref={textInput} 
+          />
+          <Button variant='primary' type='submit' className='miami'>
+            Add player
+          </Button>
+          
+        </Form.Group>
+      </Form>
+    </>
   );
 }
 
