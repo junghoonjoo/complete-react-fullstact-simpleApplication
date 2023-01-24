d439afea533051ad2a826476e3e491906ef01167
junghoonjoo <junghoonjoo@gmail.com>
2023-01-24 14:27:52 +0900

    stage 1에서의 error handling 구현
    [error,setError]를 통해 사용자 입력의 validation
    체크를 한다.

--- src/src/components/Stage_1.js
+++ src/src/components/Stage_1.js
@@ -6,20 +6,41 @@ const Stage_1 = () => {
 
   const textInput=useRef();
 
+  const [error, setError]=useState([false,'']);
 
   const handleSubmit=(e)=>{
     e.preventDefault();
     const value=textInput.current.value;
+    const validate= validateInput(value);
+
+    if(validate){
+      setError([false,'']);
+      console.log('ADD PLAYER')
+    }
+
     console.log(value);
   }
+
+  const validateInput=(value)=>{
+
+    if(value===''){
+      setError([true, 'Sorry, you need to add something']);
+      return false;
+    }
+    if(value.length<=2){
+      setError([true,'Sorry, you need 3char at least']);
+      return false;
+    }
+    return true;
+  };
+
+
   return (
     <>
       <Form onSubmit={handleSubmit} className='mt-4'>
         <Form.Group>
           <Form.Control 
-            type='text' 
-            placeholder='Add plaer name' 
-            name='player' 
+            type='text' placeholder='Add player name' name='player' 
             ref={textInput} 
           />
           <Button variant='primary' type='submit' className='miami'>
@@ -27,6 +48,12 @@ const Stage_1 = () => {
           </Button>
           
         </Form.Group>
+
+        {error[0] ? 
+          <Alert>
+            {error[1]}
+          </Alert>
+        :null}
       </Form>
     </>
   );
